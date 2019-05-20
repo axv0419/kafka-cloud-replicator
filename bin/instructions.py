24b6acaa04ba0b2396eabd8a7843f72c257b87bc
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-05-19 22:21:25 -0700

    minor edit

--- bin/instructions.py
+++ bin/instructions.py
@@ -7,8 +7,7 @@ broker_urls=[item.strip() for item in os.environ['SOURCE_BROKER_SERVERS'].split(
 
 ports = [15060 + (ix*2)  for ix in range (len(broker_urls))]
 
-print(f'''
-Using GCP Admin console  open firewall on {tgt_side_tunnel_ip} for inbound traffic from {src_side_tunnel_ip} on ports {ports}
-''' )
+print('''Using GCP Admin console  open firewall on {tgt_side_tunnel_ip} for inbound traffic from {src_side_tunnel_ip} on ports {ports}
+'''.format(ports=ports,tgt_side_tunnel_ip=tgt_side_tunnel_ip,src_side_tunnel_ip=src_side_tunnel_ip))
 
 
