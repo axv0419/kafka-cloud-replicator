32c276fd18d6f8794f63afe7a1d23f2187cf02de
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-05-19 21:59:29 -0700

    minor edit

--- bin/src_docker.py
+++ bin/src_docker.py
@@ -2,26 +2,27 @@ import os
 
 
 
+broker_urls=[item.strip() for item in os.environ['SOURCE_BROKER_SERVERS'].split(',')]
+
 out_str = ['''version: "3.0"
 services:''']
 
-broker_urls=[item.strip() for item in os.environ['SOURCE_BROKER_SERVERS'].split(',')]
 for ix in range (len(broker_urls)):
-    broker_url = broker_urls[ix]
-    port = 15060 + (ix*2)
-    out_str.append (f'''  kafka-boot-broker-{ix}:
-    image: m7dock/gotunnel:01
-    container_name: kafka-boot-broker-{ix}
-    ports:
-      - {port}:9092
-    entrypoint:
-    - /work/gotunnel
-    - -backend
-    - {broker_url}
-    - -listen 
-    - :9092 
-    - -tunnels 
-    - "0"
-    ''')
+  broker_url = broker_urls[ix]
+  port = 15060 + (ix*2)
+  container = f'''  kafka-boot-broker-{ix}:
+  image: m7dock/gotunnel:01
+  container_name: kafka-boot-broker-{ix}
+  ports:
+    - {port}:9092
+  entrypoint:
+  - /work/gotunnel
+  - -backend
+  - {broker_url}
+  - -listen 
+  - :9092 
+  - -tunnels 
+  - "0"'''
+  out_str.append(container)
 print ("\n".join(out_str))
     
